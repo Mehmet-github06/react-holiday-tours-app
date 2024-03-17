78d2a8275b8657e111ab72a53f56b7ed36c2fde3
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-17 23:55:32 +0300

    loading

---
+++ src/loading.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./loading.css"
+
+const loading = () => {
+  return (
+    <div className='loading'>
+      Loading...
+    </div>
+  )
+}
+
+export default loading
