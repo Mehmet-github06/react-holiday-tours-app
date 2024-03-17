6150fc60bc0ccbd489401f517f4ec7ac3f1edbca
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-17 23:51:54 +0300

    App css

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
+import "./App.css";
+import loading from "./loading"
+import tours from "./tours"
+import React , {useState,useEffect} from "react";
+
+const url = "http://course-api.com/react-tours-project"
+
 
 function App() {
   return (
