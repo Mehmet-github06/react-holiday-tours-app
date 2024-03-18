d01adf7beb24661b209e08a323cb89b0f910087f
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-18 23:37:05 +0300

    useState

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ const url = "http://course-api.com/react-tours-project"
 
 
 function App() {
+  const [loading,setloading] = useState(true)
+  const [tours,settours] = useState(true)
   return (
     <div className="App">
     
