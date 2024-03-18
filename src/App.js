d2b47b26de6283ea56677d3f7c6a7ea60e12e537
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-18 23:47:40 +0300

    fecth

--- src/App.js
+++ src/App.js
@@ -8,7 +8,20 @@ const url = "http://course-api.com/react-tours-project"
 
 function App() {
   const [loading,setloading] = useState(true)
-  const [tours,settours] = useState(true)
+  const [tours,settours] = useState([])
+
+const fecthTours =async()=>{
+  setloading(true)
+  try {
+    const response = await fetch(url);
+    const tours =await response.json();
+    setloading(false)
+    settours(tours)
+  } catch (error) {
+    
+  }
+}
+
   return (
     <div className="App">
     
