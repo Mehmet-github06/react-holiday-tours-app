4cedcbea55120d0639def9786fe68225724375d3
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-23 23:45:09 +0300

    removetour

--- src/tours.jsx
+++ src/tours.jsx
@@ -2,14 +2,14 @@ import React from 'react'
 import Tour from "./Tour.jsx"
 import "./Tours.css"
 
-const Tours = ({tours}) => {
+const Tours = ({tours,removeTour}) => {
   return (
     <div className='title'>
       <h2>Our Tours</h2>
       <div className='line'></div>
        {tours.map((tour)=>{
         return(
-          <Tour key={tour.id} {...tour}/>
+          <Tour key={tour.id} {...tour} removeTour={removeTour}/>
         )
        })}
     </div>
