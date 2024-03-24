b39ddd9ee22dbe5abe5abab3de4ef962683ced46
mehmet dogan <mehmetseymen91@gmail.com>
2024-03-24 15:33:13 +0300

    length

--- src/App.js
+++ src/App.js
@@ -1,44 +1,46 @@
-import "./App.css"
+import "./App.css";
 import Loading from "./Loading";
 import Tours from "./Tours";
-import React , {useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 
-const url = "http://course-api.com/react-tours-project"
-
+const url = "http://course-api.com/react-tours-project";
 
 function App() {
-  const [loading,setLoading] = useState(true)
-  const [tours,setTours] = useState([])
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
   const removeTour = (id) => {
-    const newTours = tours.filter((tour)=>tour.id !==id)
-    setTours(newTours)
-  }
-const fecthTours =async()=>{
-  setLoading(true)
-  try {
-    const response = await fetch(url);
-    const tours =await response.json();
-    setLoading(false)
-    setTours(tours)
-  } catch (error) {
-    setLoading(false);
-    console.log(error)
-  }
-}
-useEffect(()=>{
-  fecthTours()
-},[])
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+  const fecthTours = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const tours = await response.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fecthTours();
+  }, []);
 
-if(loading){
-  return(
-    <Loading/>
+  if (loading) {
+    return <Loading />;
+  }
+if (tours.length === 0) {
+  return (
+    <main>
+      
+    </main>
   )
-
 }
-
   return (
     <div className="App">
-    <Tours tours={tours} removeTour={removeTour}/>
+      <Tours tours={tours} removeTour={removeTour} />
     </div>
   );
 }
